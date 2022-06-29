9798c7c638177882932aa558ba51d7953c85cd2c
Daniel Belorini <danielbelorini267@gmail.com>
2022-06-29 01:20:10 -0300

    Ajustado mascara de precos

--- administracao/static/js/mascara.js
+++ administracao/static/js/mascara.js
@@ -1,5 +1,5 @@
 $(function() {
     $('.mask-cpf').mask('000.000.000-00', {reverse: true});
     $('.mask-telefone').mask('(00) 0000-0000');
-    $('.mask-preco').mask("#.##0,00", {reverse: true});
+    $('.mask-preco').mask("0.0000.00", {reverse: true});
 });
