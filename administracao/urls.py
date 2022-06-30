c424b192659710a6e9c03de7cf8152f6cf613e3e
Daniel Belorini <danielbelorini267@gmail.com>
2022-06-30 20:53:50 -0300

    Adicionando url das views para relatorios

--- administracao/urls.py
+++ administracao/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('deletar/<int:id_agenda>/', views.deletar, name='deletar'),
     path('edita_cliente/<int:id_cliente>/', views.edita_cliente, name='edita_cliente'),
     path('deleta_cliente/<int:id_cliente>/', views.deleta_cliente, name='deleta_cliente'),
+    path('relatorio_cliente/', views.relatorio_cliente, name='relatorio_cliente'),
+    path('relatorio_profissional/', views.relatorio_profissional, name='relatorio_profissional'),
+    path('relatorio_servico/', views.relatorio_servico, name='relatorio_servico'),
 ]
