fb46a6e82104ec647b97ff31d2ff619144cb3807
Daniel Belorini <danielbelorini267@gmail.com>
2022-06-30 22:23:18 -0300

    Adicionado campo ativo models cliente e profissionais

--- profissional/models.py
+++ profissional/models.py
@@ -8,6 +8,7 @@ from django.db import models
 class Prof(models.Model):
     nome = models.CharField('Profissional', max_length=100, blank=True)
     telefone = models.CharField('Telefone', max_length=14, blank=True)
+    ativo = models.BooleanField('Ativo', default=True)
 
     class Meta:
         verbose_name = 'Profissional'
