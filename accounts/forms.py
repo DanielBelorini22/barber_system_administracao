cb9d85aa125da0979328cac4789a27acf49e635b
Daniel Belorini <danielbelorini267@gmail.com>
2022-07-04 11:38:39 -0300

    Criado widget de senha no form de cadastro de usuario

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+from django.forms.widgets import PasswordInput
 from django import forms
 from django.contrib.auth.models import User
 
@@ -6,3 +7,6 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'password')
+        widgets = {
+            'password': PasswordInput(),
+        }
